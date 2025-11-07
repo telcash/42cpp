72fb8dd327fb68eb198e8c506e88bca971d042e8
telcash <telcash@protonmail.com>
2025-11-07 07:50:39 +0100

    Added m01ex05

---
+++ module01/ex05/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carlossalazar <carlossalazar@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 22:09:18 by carlossalaz       #+#    #+#             */
+/*   Updated: 2025/11/06 22:09:45 by carlossalaz      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+int main() {
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    harl.complain("UNKNOWN");
+
+    return 0;
+}
