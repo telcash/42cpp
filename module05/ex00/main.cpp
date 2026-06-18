038f39c914399cc4e25cb626a07e51a86255f820
telcash <telcash@protonmail.com>
2026-06-18 17:40:03 +0200

    Cambios en module05 y 06

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -3,16 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: carlossalazar <carlossalazar@student.42    +#+  +:+       +#+        */
+/*   By: telcash <telcash@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 20:56:52 by csalazar          #+#    #+#             */
-/*   Updated: 2025/11/20 07:46:13 by carlossalaz      ###   ########.fr       */
+/*   Updated: 2026/06/18 16:57:26 by telcash          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main() {
+    try {
+        Bureaucrat d;
+        std::cout << d << std::endl;
+    } catch (const std::exception& e) {
+            std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    try {
+        Bureaucrat e("John Doe", 42);
+        std::cout << e << std::endl;
+        Bureaucrat f("Jane Smith", 100);
+        std::cout << "Before assignment:" << std::endl;
+        std::cout << f << std::endl;
+        f = e;
+        std::cout << "After assignment:" << std::endl;
+        std::cout << f << std::endl;
+        Bureaucrat g(e);
+        std::cout << g << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    
     try {
         Bureaucrat b1("Boss", 2);
         std::cout << b1 << std::endl;
@@ -34,13 +56,13 @@ int main() {
     }
 
     try {
-        Bureaucrat b3("BHigh", 0);  // This should throw an exception
+        Bureaucrat b3("BHigh", 0);
     } catch (const std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
 
     try {
-        Bureaucrat b4("BLow", 151);  // This should throw an exception
+        Bureaucrat b4("BLow", 151);
     } catch (const std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
